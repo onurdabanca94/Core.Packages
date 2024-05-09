47dd3ddb89372bbb111300a31ef52277ae40ad27
Onur Dabanca <onurdbnc@gmail.com>
2024-05-09 05:25:07 +0300

    Custom validator inserted.

--- Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
@@ -8,10 +8,12 @@ public abstract class ExceptionHandler
         exception switch
         {
             BusinessException businessException => HandleException(businessException), // Eğer exception tanımladığımız exception ise yani: businessException ise
+            ValidationException validationException => HandleException(validationException),
             _ => HandleException(exception) // Tanımlanan exception haricinde bir exception gelirse yukarıdaki exception'ı handle et.
         };
 
     protected abstract Task HandleException(BusinessException businessException); //ExceptionHandler'ı inherit eden sınıf buradakini doldurmak zorunda olsun. Implement etsin.
+    protected abstract Task HandleException(ValidationException validationException);
     protected abstract Task HandleException(Exception exception); //Eğer exception businessException değilse bu çalışacak.
 
 }
