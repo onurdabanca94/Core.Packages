ab7a243e68d3cf6b9a0824884c8daf28744a1af5
Onur Dabanca <onurdbnc@gmail.com>
2024-05-10 04:36:46 +0300

    Cache removing inserted.

---
+++ Core.Application/Pipelines/Caching/CacheRemovingBehavior.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Core.Application.Pipelines.Caching;
+
+public class CacheRemovingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>, ICacheRemoverRequest
+{
+    private readonly IDistributedCache _cache;
+
+    public CacheRemovingBehavior(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (request.BypassCache)
+        {
+            return await next();
+        }
+
+        TResponse response = await next();
+
+        if (request.CacheKey != null)
+        {
+            await _cache.RefreshAsync(request.CacheKey, cancellationToken);
+        }
+
+        return response;
+    }
+}
