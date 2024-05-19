9074271ebb0980e0fa9798dcc5b604580a7b0faf
Onur Dabanca <onurdbnc@gmail.com>
2024-05-19 04:53:14 +0300

    Serilog base logging operations created. FileLogging inserted.

---
+++ Core.CrossCuttingConcerns/Logging/LogDetail.cs
@@ -0,0 +1,25 @@
+﻿namespace Core.CrossCuttingConcerns.Logging;
+
+public class LogDetail
+{
+    public string Fullname { get; set; }
+    public string MethodName { get; set; }
+    public string User { get; set; }
+    public List<LogParameter> Parameters { get; set; }
+
+    public LogDetail()
+    {
+        Fullname = string.Empty;
+        MethodName = string.Empty;
+        User = string.Empty;
+        Parameters = new List<LogParameter>();
+    }
+
+    public LogDetail(string fullName, string methodName, string user, List<LogParameter> parameters)
+    {
+        fullName = fullName;
+        MethodName = methodName;
+        User = user;
+        Parameters = parameters;
+    }
+}
